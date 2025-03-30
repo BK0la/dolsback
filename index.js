7496202adbe683143ac55bff877e1e6372ea7ea7
BK0la <marat.khudych@nure.ua>
2025-03-30 14:24:37 +0200

    connect mongodb

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express'
-
+import mongoose from 'mongoose'
 const PORT = 7878;
+const DB_URL = 'mongodb+srv://admin:admindolsas@cluster0.09zilly.mongodb.net/'
 const app = express()
 
 app.get('/', (req, res) => {
     res.status(200).json('Server work')
 })
-app.listen(PORT, ()=>console.log('server started on port'+ PORT))
+
+async function startApp(){
+    try{
+        await mongoose.connect(DB_URL)
+        app.listen(PORT, ()=>console.log('server started on port'+ PORT))
+    } catch(e){
+        console.log(e)
+    }
+}
+    
+startApp()

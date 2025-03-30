70314f704f7b93397d6c74095e16835c068a65d7
BK0la <marat.khudych@nure.ua>
2025-03-30 14:13:54 +0200

    + mongoos +nodemon

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('SERVER WORK')
+import express from 'express'
+
+const PORT = 7878;
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json('Server work')
+})
+app.listen(PORT, ()=>console.log('server started on port'+ PORT))
